173f096919b525c7907ae23b15633c05ad2ccc94
Sergey <duxxless53@ya.ru>
2022-07-05 22:04:00 +0300

    Minor update.

--- FirebaseToDo/Helpers/Extension UIColor.swift
+++ FirebaseToDo/Helpers/Extension UIColor.swift
@@ -9,7 +9,6 @@ extension UIColor {
     static func myGray() -> UIColor {
         return UIColor(named: "myGray")!
     }
-    
     static func colorArray() -> [UIColor] {
         let colors: [UIColor] = [.systemRed, .systemBlue, .systemGreen, .systemBrown, .systemIndigo, .systemTeal, .systemYellow, .systemCyan, .systemMint, .systemRed, .systemBlue, .systemPink, .systemBrown, .systemIndigo, .systemPurple, .systemTeal, .systemPurple, .systemYellow, .systemCyan, .systemMint, .systemGreen, .systemRed, .systemBlue, .systemGreen, .systemBrown, .systemIndigo, .systemTeal, .systemYellow, .systemCyan, .systemMint, .systemRed, .systemBlue, .systemPink, .systemBrown, .systemIndigo, .systemPurple, .systemTeal, .systemPurple, .systemYellow, .systemCyan, .systemMint, .systemGreen]
         return colors
